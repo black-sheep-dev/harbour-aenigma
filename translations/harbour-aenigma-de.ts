ac04f6b3beeebb798839ed7e18b0b6f65b1bd020
Weblate <noreply@weblate.org>
2021-12-29 18:21:21 +0000

    Translated using Weblate (German)
    Currently translated at 100.0% (31 of 31 strings)
    
    Co-authored-by: Weblate Admin <blacksheep@nubecula.org>
    Translate-URL: http://weblate.nubecula.org/projects/harbour-aenigma/harbour-aenigma/de/
    Translation: harbour-aenigma/harbour-aenigma

--- translations/harbour-aenigma-de.ts
+++ translations/harbour-aenigma-de.ts
@@ -1,133 +1,134 @@
 <?xml version="1.0" encoding="utf-8"?>
 <!DOCTYPE TS>
-<TS version="2.1">
+<TS version="2.1" language="de">
 <context>
     <name></name>
     <message id="id-about">
         <source>About</source>
-        <translation type="unfinished"></translation>
+        <translation>Über</translation>
     </message>
     <message id="id-app-name">
         <source>Aenigma</source>
-        <translation type="unfinished"></translation>
+        <translation>Aenigma</translation>
     </message>
     <message id="id-about-page-description">
         <source>Aenigma is a Sudoku puzzle game for Sailfish OS.</source>
-        <translation type="unfinished"></translation>
+        <translation>Aenigma ist ein Sudoku Puzzle Spiel für Sailfish OS.</translation>
     </message>
     <message id="id-translations">
         <source>Translations</source>
-        <translation type="unfinished"></translation>
+        <translation>Übersetzungen</translation>
     </message>
     <message id="id-about-page-translation-text">
         <source>Your language is not available? You are welcome to support this project by translating it on my self hosted Weblate server.</source>
-        <translation type="unfinished"></translation>
+        <translation>Deine Sprache ist nicht verfügbar? Du kannst das Projekt gerne unterstützen, indem Du eine Übersetzung auf meinen selbst gehosteten Weblate Server machst.</translation>
     </message>
     <message id="id-sources">
         <source>Sources</source>
-        <translation type="unfinished"></translation>
+        <translation>Quellen</translation>
     </message>
     <message id="id-donations">
         <source>Donations</source>
-        <translation type="unfinished"></translation>
+        <translation>Spenden</translation>
     </message>
     <message id="id-buy-me-a-beer">
         <source>If you like my work why not buy me a beer?</source>
-        <translation type="unfinished"></translation>
+        <translation>Wenn Du meine Arbeit magst, warum gibst du mir nicht einfach ein Bier aus?</translation>
     </message>
     <message id="id-donate-paypal">
         <source>Donate with PayPal</source>
-        <translation type="unfinished"></translation>
+        <translation>Spenden via PayPal</translation>
     </message>
     <message id="id-donate-liberpay">
         <source>Donate with Liberpay</source>
-        <translation type="unfinished"></translation>
+        <translation>Spenden via Liberpay</translation>
     </message>
     <message id="id-reset">
         <source>Reset</source>
-        <translation type="unfinished"></translation>
+        <translation>Zurücksetzen</translation>
     </message>
     <message id="id-new-game">
         <source>New game</source>
-        <translation type="unfinished"></translation>
+        <translation>Neues Spiel</translation>
     </message>
     <message id="id-placeholder-text">
         <source>Want to play?</source>
-        <translation type="unfinished"></translation>
+        <translation>Du willst spielen?</translation>
     </message>
     <message id="id-placeholder-hint">
         <source>Pull down to start a new game</source>
-        <translation type="unfinished"></translation>
+        <translation>Nach unten ziehen um ein neues Spiel zu beginnen</translation>
     </message>
     <message id="id-reset-game">
         <source>Reset game</source>
-        <translation type="unfinished"></translation>
+        <translation>Spiel zurücksetzen</translation>
     </message>
     <message id="id-generating">
         <source>Generating</source>
-        <translation type="unfinished"></translation>
+        <translation>Generiere</translation>
     </message>
     <message id="id-cells-unsolved" numerus="yes">
         <source>%n cell(s) unsolved</source>
-        <translation type="unfinished">
-            <numerusform></numerusform>
+        <translation>
+            <numerusform>%n Zelle ungelöst</numerusform>
+            <numerusform>%n Zellen ungelöst</numerusform>
         </translation>
     </message>
     <message id="id-has-errors">
         <source>There are errors</source>
-        <translation type="unfinished"></translation>
+        <translation>Es gibt Fehler</translation>
     </message>
     <message id="id-solved">
         <source>Solved</source>
-        <translation type="unfinished"></translation>
+        <translation>Gelöst</translation>
     </message>
     <message id="id-generate">
         <source>Generate</source>
-        <translation type="unfinished"></translation>
+        <translation>Generiere</translation>
     </message>
     <message id="id-dialog-new-game-info">
         <source>You can select a level of difficulty for your Sudoku puzzle.</source>
-        <translation type="unfinished"></translation>
+        <translation>Du kannst einen Schwierigkeitsgrad für Dein Sudoku Puzzle wählen.</translation>
     </message>
     <message id="id-easy">
         <source>Easy</source>
-        <translation type="unfinished"></translation>
+        <translation>Leicht</translation>
     </message>
     <message id="id-medium">
         <source>Medium</source>
-        <translation type="unfinished"></translation>
+        <translation>Mittel</translation>
     </message>
     <message id="id-hard">
         <source>Hard</source>
-        <translation type="unfinished"></translation>
+        <translation>Schwer</translation>
     </message>
     <message id="id-insane">
         <source>Insane</source>
-        <translation type="unfinished"></translation>
+        <translation>Verrückt</translation>
     </message>
     <message id="id-level-of-difficulty">
         <source>Level of difficulty</source>
-        <translation type="unfinished"></translation>
+        <translation>Schwierigkeitsgrad</translation>
     </message>
     <message id="id-settings">
         <source>Settings</source>
-        <translation type="unfinished"></translation>
+        <translation>Einstellungen</translation>
     </message>
     <message id="id-support-tools">
         <source>Support tools</source>
-        <translation type="unfinished"></translation>
+        <translation>Hilfsmittel</translation>
     </message>
     <message id="id-highlighting">
         <source>Highlighting</source>
-        <translation type="unfinished"></translation>
+        <translation>Hervorhebung</translation>
     </message>
     <message id="id-highlighting-desc">
         <source>This option enables highlighting of rows, columns and boxes already blocked by the selected number.</source>
-        <translation type="unfinished"></translation>
+        <translation>Diese Option aktiviert die Hervorhebung von Reihen, Spalten und Blöcken, die bereits von der ausgewählten Zahl blockiert sind.</translation>
     </message>
     <message id="id-sudoku-board">
         <source>Sudoku board</source>
-        <translation type="unfinished"></translation>
+        <translation>Sudoku Board</translation>
     </message>
 </context>
 <context>
