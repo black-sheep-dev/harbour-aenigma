587f179c9262eec61e123a3955d3b068ed2a65db
Samuel Kron <blacksheep@nubecula.org>
2021-12-29 19:13:57 +0100

    Update translation files

--- translations/harbour-aenigma.ts
+++ translations/harbour-aenigma.ts
@@ -1,133 +1,134 @@
 <?xml version="1.0" encoding="utf-8"?>
 <!DOCTYPE TS>
-<TS version="2.1">
+<TS version="2.1" language="en_US">
 <context>
     <name></name>
     <message id="id-about">
         <source>About</source>
-        <translation type="unfinished"></translation>
+        <translation>About</translation>
     </message>
     <message id="id-app-name">
         <source>Aenigma</source>
-        <translation type="unfinished"></translation>
+        <translation>Aenigma</translation>
     </message>
     <message id="id-about-page-description">
         <source>Aenigma is a Sudoku puzzle game for Sailfish OS.</source>
-        <translation type="unfinished"></translation>
+        <translation>Aenigma is a Sudoku puzzle game for Sailfish OS.</translation>
     </message>
     <message id="id-translations">
         <source>Translations</source>
-        <translation type="unfinished"></translation>
+        <translation>Translations</translation>
     </message>
     <message id="id-about-page-translation-text">
         <source>Your language is not available? You are welcome to support this project by translating it on my self hosted Weblate server.</source>
-        <translation type="unfinished"></translation>
+        <translation>Your language is not available? You are welcome to support this project by translating it on my self hosted Weblate server.</translation>
     </message>
     <message id="id-sources">
         <source>Sources</source>
-        <translation type="unfinished"></translation>
+        <translation>Sources</translation>
     </message>
     <message id="id-donations">
         <source>Donations</source>
-        <translation type="unfinished"></translation>
+        <translation>Donations</translation>
     </message>
     <message id="id-buy-me-a-beer">
         <source>If you like my work why not buy me a beer?</source>
-        <translation type="unfinished"></translation>
+        <translation>If you like my work why not buy me a beer?</translation>
     </message>
     <message id="id-donate-paypal">
         <source>Donate with PayPal</source>
-        <translation type="unfinished"></translation>
+        <translation>Donate with PayPal</translation>
     </message>
     <message id="id-donate-liberpay">
         <source>Donate with Liberpay</source>
-        <translation type="unfinished"></translation>
+        <translation>Donate with Liberpay</translation>
     </message>
     <message id="id-reset">
         <source>Reset</source>
-        <translation type="unfinished"></translation>
+        <translation>Reset</translation>
     </message>
     <message id="id-new-game">
         <source>New game</source>
-        <translation type="unfinished"></translation>
+        <translation>New game</translation>
     </message>
     <message id="id-placeholder-text">
         <source>Want to play?</source>
-        <translation type="unfinished"></translation>
+        <translation>Want to play?</translation>
     </message>
     <message id="id-placeholder-hint">
         <source>Pull down to start a new game</source>
-        <translation type="unfinished"></translation>
+        <translation>Pull down to start a new game</translation>
     </message>
     <message id="id-reset-game">
         <source>Reset game</source>
-        <translation type="unfinished"></translation>
+        <translation>Reset game</translation>
     </message>
     <message id="id-generating">
         <source>Generating</source>
-        <translation type="unfinished"></translation>
+        <translation>Generating</translation>
     </message>
     <message id="id-cells-unsolved" numerus="yes">
         <source>%n cell(s) unsolved</source>
-        <translation type="unfinished">
-            <numerusform></numerusform>
+        <translation>
+            <numerusform>%n cell unsolved</numerusform>
+            <numerusform>%n cells unsolved</numerusform>
         </translation>
     </message>
     <message id="id-has-errors">
         <source>There are errors</source>
-        <translation type="unfinished"></translation>
+        <translation>There are errors</translation>
     </message>
     <message id="id-solved">
         <source>Solved</source>
-        <translation type="unfinished"></translation>
+        <translation>Solved</translation>
     </message>
     <message id="id-generate">
         <source>Generate</source>
-        <translation type="unfinished"></translation>
+        <translation>Generate</translation>
     </message>
     <message id="id-dialog-new-game-info">
         <source>You can select a level of difficulty for your Sudoku puzzle.</source>
-        <translation type="unfinished"></translation>
+        <translation>You can select a level of difficulty for your Sudoku puzzle.</translation>
     </message>
     <message id="id-easy">
         <source>Easy</source>
-        <translation type="unfinished"></translation>
+        <translation>Easy</translation>
     </message>
     <message id="id-medium">
         <source>Medium</source>
-        <translation type="unfinished"></translation>
+        <translation>Medium</translation>
     </message>
     <message id="id-hard">
         <source>Hard</source>
-        <translation type="unfinished"></translation>
+        <translation>Hard</translation>
     </message>
     <message id="id-insane">
         <source>Insane</source>
-        <translation type="unfinished"></translation>
+        <translation>Insane</translation>
     </message>
     <message id="id-level-of-difficulty">
         <source>Level of difficulty</source>
-        <translation type="unfinished"></translation>
+        <translation>Level of difficulty</translation>
     </message>
     <message id="id-settings">
         <source>Settings</source>
-        <translation type="unfinished"></translation>
+        <translation>Settings</translation>
     </message>
     <message id="id-support-tools">
         <source>Support tools</source>
-        <translation type="unfinished"></translation>
+        <translation>Support tools</translation>
     </message>
     <message id="id-highlighting">
         <source>Highlighting</source>
-        <translation type="unfinished"></translation>
+        <translation>Highlighting</translation>
     </message>
     <message id="id-highlighting-desc">
         <source>This option enables highlighting of rows, columns and boxes already blocked by the selected number.</source>
-        <translation type="unfinished"></translation>
+        <translation>This option enables highlighting of rows, columns and boxes already blocked by the selected number.</translation>
     </message>
     <message id="id-sudoku-board">
         <source>Sudoku board</source>
-        <translation type="unfinished"></translation>
+        <translation>Sudoku board</translation>
     </message>
 </context>
 </TS>
