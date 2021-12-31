c4e57724b11f04940ca5ce6978c502002ab84f3a
Weblate <noreply@weblate.org>
2021-12-31 09:30:19 +0000

    Translated using Weblate (Spanish)
    Currently translated at 100.0% (39 of 39 strings)
    
    Co-authored-by: Carmen F. B <carmenfdezb@mail.com>
    Translate-URL: http://weblate.nubecula.org/projects/harbour-aenigma/harbour-aenigma/es/
    Translation: harbour-aenigma/harbour-aenigma

--- translations/harbour-aenigma-es.ts
+++ translations/harbour-aenigma-es.ts
@@ -5,164 +5,164 @@
     <name></name>
     <message id="id-about">
         <source>About</source>
-        <translation type="unfinished"></translation>
+        <translation>Acerca de</translation>
     </message>
     <message id="id-app-name">
         <source>Aenigma</source>
-        <translation type="unfinished"></translation>
+        <translation>Aenigma</translation>
     </message>
     <message id="id-about-page-description">
         <source>Aenigma is a Sudoku puzzle game for Sailfish OS.</source>
-        <translation type="unfinished"></translation>
+        <translation>Aenigma es un juego de sudoku para Sailfish OS.</translation>
     </message>
     <message id="id-translations">
         <source>Translations</source>
-        <translation type="unfinished"></translation>
+        <translation>Traducciones</translation>
     </message>
     <message id="id-about-page-translation-text">
         <source>Your language is not available? You are welcome to support this project by translating it on my self hosted Weblate server.</source>
-        <translation type="unfinished"></translation>
+        <translation>¿Tu idioma no está disponible? Te animo a apoyar este proyecto traduciéndolo en mi propio servidor Weblate.</translation>
     </message>
     <message id="id-sources">
         <source>Sources</source>
-        <translation type="unfinished"></translation>
+        <translation>Fuente</translation>
     </message>
     <message id="id-donations">
         <source>Donations</source>
-        <translation type="unfinished"></translation>
+        <translation>Donaciones</translation>
     </message>
     <message id="id-buy-me-a-beer">
         <source>If you like my work why not buy me a beer?</source>
-        <translation type="unfinished"></translation>
+        <translation>Si te gusta mi trabajo, ¿por qué no me invitas a una cerveza?</translation>
     </message>
     <message id="id-donate-paypal">
         <source>Donate with PayPal</source>
-        <translation type="unfinished"></translation>
+        <translation>Donar con PayPal</translation>
     </message>
     <message id="id-donate-liberpay">
         <source>Donate with Liberpay</source>
-        <translation type="unfinished"></translation>
+        <translation>Donar con Liberpay</translation>
     </message>
     <message id="id-reset">
         <source>Reset</source>
-        <translation type="unfinished"></translation>
+        <translation>Reiniciar</translation>
     </message>
     <message id="id-new-game">
         <source>New game</source>
-        <translation type="unfinished"></translation>
+        <translation>Partida nueva</translation>
     </message>
     <message id="id-placeholder-text">
         <source>Want to play?</source>
-        <translation type="unfinished"></translation>
+        <translation>¿Quieres jugar?</translation>
     </message>
     <message id="id-placeholder-hint">
         <source>Pull down to start a new game</source>
-        <translation type="unfinished"></translation>
+        <translation>Desliza hacia abajo para iniciar una partida nueva</translation>
     </message>
     <message id="id-reset-game">
         <source>Reset game</source>
-        <translation type="unfinished"></translation>
+        <translation>Reiniciar partida</translation>
     </message>
     <message id="id-generating">
         <source>Generating</source>
-        <translation type="unfinished"></translation>
+        <translation>Generando</translation>
     </message>
     <message id="id-cells-unsolved" numerus="yes">
         <source>%n cell(s) unsolved</source>
-        <translation type="unfinished">
-            <numerusform></numerusform>
-            <numerusform></numerusform>
+        <translation>
+            <numerusform>%n celda sin resolver</numerusform>
+            <numerusform>%n celdas sin resolver</numerusform>
         </translation>
     </message>
     <message id="id-has-errors">
         <source>There are errors</source>
-        <translation type="unfinished"></translation>
+        <translation>Hay errores</translation>
     </message>
     <message id="id-solved">
         <source>Solved</source>
-        <translation type="unfinished"></translation>
+        <translation>Resuelto</translation>
     </message>
     <message id="id-generate">
         <source>Generate</source>
-        <translation type="unfinished"></translation>
+        <translation>Generar</translation>
     </message>
     <message id="id-dialog-new-game-info">
         <source>You can select a level of difficulty for your Sudoku puzzle.</source>
-        <translation type="unfinished"></translation>
+        <translation>Puedes seleccionar el nivel de dificultad de tu sudoku.</translation>
     </message>
     <message id="id-easy">
         <source>Easy</source>
-        <translation type="unfinished"></translation>
+        <translation>Fácil</translation>
     </message>
     <message id="id-medium">
         <source>Medium</source>
-        <translation type="unfinished"></translation>
+        <translation>Medio</translation>
     </message>
     <message id="id-hard">
         <source>Hard</source>
-        <translation type="unfinished"></translation>
+        <translation>Difícil</translation>
     </message>
     <message id="id-insane">
         <source>Insane</source>
-        <translation type="unfinished"></translation>
+        <translation>Muy difícil</translation>
     </message>
     <message id="id-level-of-difficulty">
         <source>Level of difficulty</source>
-        <translation type="unfinished"></translation>
+        <translation>Nivel de dificultad</translation>
     </message>
     <message id="id-settings">
         <source>Settings</source>
-        <translation type="unfinished"></translation>
+        <translation>Ajustes</translation>
     </message>
     <message id="id-support-tools">
         <source>Support tools</source>
-        <translation type="unfinished"></translation>
+        <translation>Herramientas de ayuda</translation>
     </message>
     <message id="id-highlighting">
         <source>Highlighting</source>
         <oldsource>Prevent display blanking</oldsource>
-        <translation type="unfinished"></translation>
+        <translation>Resaltado</translation>
     </message>
     <message id="id-highlighting-desc">
         <source>This option enables highlighting of rows, columns and boxes already blocked by the selected number.</source>
         <oldsource>This option prevents display blanking during playing.</oldsource>
-        <translation type="unfinished"></translation>
+        <translation>Esta opción permite resaltar filas, columnas y casillas que ya están bloqueadas por el número seleccionado.</translation>
     </message>
     <message id="id-sudoku-board">
         <source>Sudoku board</source>
-        <translation type="unfinished"></translation>
+        <translation>Tablero de sudoku</translation>
     </message>
     <message id="id-level">
         <source>Level</source>
-        <translation type="unfinished"></translation>
+        <translation>Nivel</translation>
     </message>
     <message id="id-general">
         <source>General</source>
-        <translation type="unfinished"></translation>
+        <translation>General</translation>
     </message>
     <message id="id-auto-notes">
         <source>Auto notes</source>
-        <translation type="unfinished"></translation>
+        <translation>Notas automáticas</translation>
     </message>
     <message id="id-auto-notes-desc">
         <source>This option automatically adds all notes to a new sudoku puzzle.</source>
-        <translation type="unfinished"></translation>
+        <translation>Esta opción añade automáticamente todas las notas a tu nuevo sudoku.</translation>
     </message>
     <message id="id-display-blanking">
         <source>Prevent display blanking</source>
-        <translation type="unfinished"></translation>
+        <translation>Evitar el apagado de pantalla</translation>
     </message>
     <message id="id-display-blanking-desc">
         <source>This option prevents display blanking during playing.</source>
-        <translation type="unfinished"></translation>
+        <translation>Esta opción evita que se apague la pantalla durante la partida.</translation>
     </message>
     <message id="id-auto-clean">
         <source>Auto clean</source>
-        <translation type="unfinished"></translation>
+        <translation>Borrado automático</translation>
     </message>
     <message id="id-auto-clean-desc">
         <source>This option enables auto cleaning of notes when a number is added to a cell.</source>
-        <translation type="unfinished"></translation>
+        <translation>Esta opción permite el borrado automático de las notas cuando se añade un número a la casilla.</translation>
     </message>
 </context>
 </TS>
