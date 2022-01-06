4824ab5bd626078153c3f787d87490aeddc78692
Weblate <noreply@weblate.org>
2022-01-06 11:33:37 +0000

    Translated using Weblate (Dutch)
    Currently translated at 100.0% (54 of 54 strings)
    
    Co-authored-by: richard grooff <rgrnetalk@yahoo.com>
    Translate-URL: http://weblate.nubecula.org/projects/harbour-aenigma/harbour-aenigma/nl/
    Translation: harbour-aenigma/harbour-aenigma

--- translations/harbour-aenigma-nl.ts
+++ translations/harbour-aenigma-nl.ts
@@ -5,123 +5,123 @@
     <name></name>
     <message id="id-about">
         <source>About</source>
-        <translation type="unfinished"></translation>
+        <translation>Over</translation>
     </message>
     <message id="id-app-name">
         <source>Aenigma</source>
-        <translation type="unfinished"></translation>
+        <translation>Enigma</translation>
     </message>
     <message id="id-about-page-description">
         <source>Aenigma is a Sudoku puzzle game for Sailfish OS.</source>
-        <translation type="unfinished"></translation>
+        <translation>Enigma is een Sudoku puzzel spel voor Sailfish OS.</translation>
     </message>
     <message id="id-translations">
         <source>Translations</source>
-        <translation type="unfinished"></translation>
+        <translation>Vertalingen</translation>
     </message>
     <message id="id-about-page-translation-text">
         <source>Your language is not available? You are welcome to support this project by translating it on my self hosted Weblate server.</source>
-        <translation type="unfinished"></translation>
+        <translation>Is jouw taal niet beschikbaar? Je kunt dit project ondersteunen door het te vertalen op mijn self-hosted Weblate server.</translation>
     </message>
     <message id="id-sources">
         <source>Sources</source>
-        <translation type="unfinished"></translation>
+        <translation>Bronnen</translation>
     </message>
     <message id="id-donations">
         <source>Donations</source>
-        <translation type="unfinished"></translation>
+        <translation>Donaties</translation>
     </message>
     <message id="id-buy-me-a-beer">
         <source>If you like my work why not buy me a beer?</source>
-        <translation type="unfinished"></translation>
+        <translation>Geniet je van mijn werk, trakteer je mij dan op een biertje?</translation>
     </message>
     <message id="id-donate-paypal">
         <source>Donate with PayPal</source>
-        <translation type="unfinished"></translation>
+        <translation>Doneer met PayPal</translation>
     </message>
     <message id="id-donate-liberpay">
         <source>Donate with Liberpay</source>
-        <translation type="unfinished"></translation>
+        <translation>Doneer met Librepay</translation>
     </message>
     <message id="id-reset">
         <source>Reset</source>
-        <translation type="unfinished"></translation>
+        <translation>Herstellen</translation>
     </message>
     <message id="id-new-game">
         <source>New game</source>
-        <translation type="unfinished"></translation>
+        <translation>Nieuw spel</translation>
     </message>
     <message id="id-placeholder-text">
         <source>Want to play?</source>
-        <translation type="unfinished"></translation>
+        <translation>Wil je spelen?</translation>
     </message>
     <message id="id-placeholder-hint">
         <source>Pull down to start a new game</source>
-        <translation type="unfinished"></translation>
+        <translation>Pull down om een nieuw spel te starten</translation>
     </message>
     <message id="id-reset-game">
         <source>Reset game</source>
-        <translation type="unfinished"></translation>
+        <translation>Herstel het spel</translation>
     </message>
     <message id="id-generating">
         <source>Generating</source>
-        <translation type="unfinished"></translation>
+        <translation>Aan het genereren</translation>
     </message>
     <message id="id-cells-unsolved" numerus="yes">
         <source>%n cell(s) unsolved</source>
-        <translation type="unfinished">
-            <numerusform></numerusform>
-            <numerusform></numerusform>
+        <translation>
+            <numerusform>%n cel onopgelost</numerusform>
+            <numerusform>%n cellen onopgelost</numerusform>
         </translation>
     </message>
     <message id="id-has-errors">
         <source>There are errors</source>
-        <translation type="unfinished"></translation>
+        <translation>Er zijn fouten</translation>
     </message>
     <message id="id-solved">
         <source>Solved</source>
-        <translation type="unfinished"></translation>
+        <translation>Opgelost</translation>
     </message>
     <message id="id-generate">
         <source>Generate</source>
-        <translation type="unfinished"></translation>
+        <translation>Genereer</translation>
     </message>
     <message id="id-dialog-new-game-info">
         <source>You can select a level of difficulty for your Sudoku puzzle.</source>
-        <translation type="unfinished"></translation>
+        <translation>Je kan een moeilijkheidsgraad van je Sudoku puzzel kiezen.</translation>
     </message>
     <message id="id-easy">
         <source>Easy</source>
-        <translation type="unfinished"></translation>
+        <translation>Makkelijk</translation>
     </message>
     <message id="id-medium">
         <source>Medium</source>
-        <translation type="unfinished"></translation>
+        <translation>Gemiddeld</translation>
     </message>
     <message id="id-hard">
         <source>Hard</source>
-        <translation type="unfinished"></translation>
+        <translation>Moeilijk</translation>
     </message>
     <message id="id-insane">
         <source>Insane</source>
-        <translation type="unfinished"></translation>
+        <translation>Onmogelijk</translation>
     </message>
     <message id="id-level-of-difficulty">
         <source>Level of difficulty</source>
-        <translation type="unfinished"></translation>
+        <translation>Moeilijkheidsgraad</translation>
     </message>
     <message id="id-settings">
         <source>Settings</source>
-        <translation type="unfinished"></translation>
+        <translation>Instellingen</translation>
     </message>
     <message id="id-support-tools">
         <source>Support tools</source>
-        <translation type="unfinished"></translation>
+        <translation>Support gereedschappen</translation>
     </message>
     <message id="id-highlighting">
         <source>Highlighting</source>
         <oldsource>Prevent display blanking</oldsource>
-        <translation type="unfinished"></translation>
+        <translation>Markering</translation>
     </message>
     <message id="id-highlighting-desc">
         <source>This option enables highlighting of rows, columns and boxes already blocked by the selected number.</source>
@@ -130,104 +130,104 @@
     </message>
     <message id="id-sudoku-board">
         <source>Sudoku board</source>
-        <translation type="unfinished"></translation>
+        <translation>Sudoku bord</translation>
     </message>
     <message id="id-level">
         <source>Level</source>
-        <translation type="unfinished"></translation>
+        <translation>Niveau</translation>
     </message>
     <message id="id-general">
         <source>General</source>
-        <translation type="unfinished"></translation>
+        <translation>Algemeen</translation>
     </message>
     <message id="id-auto-notes">
         <source>Auto notes</source>
-        <translation type="unfinished"></translation>
+        <translation>Automatische notities</translation>
     </message>
     <message id="id-auto-notes-desc">
         <source>This option automatically adds all notes to a new sudoku puzzle.</source>
-        <translation type="unfinished"></translation>
+        <translation>Deze optie voegt automatische notities toe aan een nieuwe Sudoku puzzel.</translation>
     </message>
     <message id="id-display-blanking">
         <source>Prevent display blanking</source>
-        <translation type="unfinished"></translation>
+        <translation>Voorkom time-out van het beeldscherm</translation>
     </message>
     <message id="id-display-blanking-desc">
         <source>This option prevents display blanking during playing.</source>
-        <translation type="unfinished"></translation>
+        <translation>Deze optie voorkomt de time-out van het beeldscherm tijdens het spel.</translation>
     </message>
     <message id="id-auto-clean">
         <source>Auto clean</source>
-        <translation type="unfinished"></translation>
+        <translation>Automatisch opschonen</translation>
     </message>
     <message id="id-auto-clean-desc">
         <source>This option enables auto cleaning of notes when a number is added to a cell.</source>
-        <translation type="unfinished"></translation>
+        <translation>Deze optie zet automatisch opschonen van notities aan als een nummer wordt ingevuld in een cel.</translation>
     </message>
     <message id="id-highlight-combo-desc">
         <source>Choose a highlighting mode to highlight a single cell with the selected number or the row, column and block the selected number affects.</source>
-        <translation type="unfinished"></translation>
+        <translation>Kies een methode voor markering een enkele cel met het gekozen cijfer of de rij, kolom en blok die door het gekozen nummer worden beïnvloed.</translation>
     </message>
     <message id="id-none">
         <source>None</source>
-        <translation type="unfinished"></translation>
+        <translation>Geen</translation>
     </message>
     <message id="id-cell">
         <source>Cell</source>
-        <translation type="unfinished"></translation>
+        <translation>Cel</translation>
     </message>
     <message id="id-row-column-block">
         <source>Row / column / block</source>
-        <translation type="unfinished"></translation>
+        <translation>Rij / kolom / blok</translation>
     </message>
     <message id="id-styles-desc">
         <source>Choose a style for you sudoku board.</source>
-        <translation type="unfinished"></translation>
+        <translation>Kies een stijl voor jou Sudoku bord.</translation>
     </message>
     <message id="id-style">
         <source>Style</source>
-        <translation type="unfinished"></translation>
+        <translation>Stijl</translation>
     </message>
     <message id="id-default">
         <source>Default</source>
-        <translation type="unfinished"></translation>
+        <translation>Default</translation>
     </message>
     <message id="id-style-black-and-white">
         <source>Black &amp; White</source>
-        <translation type="unfinished"></translation>
+        <translation>Zwart &amp; Wit</translation>
     </message>
     <message id="id-style-paper">
         <source>Paper</source>
-        <translation type="unfinished"></translation>
+        <translation>Papier</translation>
     </message>
     <message id="id-puzzle-solved">
         <source>Puzzle solved!</source>
-        <translation type="unfinished"></translation>
+        <translation>Puzzel opgelost!</translation>
     </message>
     <message id="id-steps-required">
         <source>Steps required</source>
-        <translation type="unfinished"></translation>
+        <translation>Aantal benodigde stappen</translation>
     </message>
     <message id="id-time-required">
         <source>Time required</source>
-        <translation type="unfinished"></translation>
+        <translation>Benodigde tijd</translation>
     </message>
     <message id="id-hints-used">
         <source>Hints used</source>
-        <translation type="unfinished"></translation>
+        <translation>Aantal gebruikte hints</translation>
     </message>
     <message id="id-generator-failed-msg">
         <source>Generation of Sudoku game failed! Please try again!</source>
         <oldsource>Generation of game failed! Please try again!</oldsource>
-        <translation type="unfinished"></translation>
+        <translation>Generatie van een Sudoku spel mislukt! Probeer het nog een keer!</translation>
     </message>
     <message id="id-reset-settings">
         <source>Reset settings</source>
-        <translation type="unfinished"></translation>
+        <translation>Reset instellingen</translation>
     </message>
     <message id="id-resetting-settings">
         <source>Resetting settings</source>
-        <translation type="unfinished"></translation>
+        <translation>Herstel instellingen</translation>
     </message>
 </context>
 </TS>
