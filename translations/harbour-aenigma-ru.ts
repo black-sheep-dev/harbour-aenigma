f82995117b2b4c8c4d638ea1330e6b086f1c45fe
Weblate <noreply@weblate.org>
2022-01-08 11:05:38 +0000

    Added translation using Weblate (Russian)
    Co-authored-by: potap14108 <potap14108@mail.ru>

---
+++ translations/harbour-aenigma-ru.ts
@@ -0,0 +1,234 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="ru">
+<context>
+    <name></name>
+    <message id="id-about">
+        <source>About</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message id="id-app-name">
+        <source>Aenigma</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message id="id-about-page-description">
+        <source>Aenigma is a Sudoku puzzle game for Sailfish OS.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message id="id-translations">
+        <source>Translations</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message id="id-about-page-translation-text">
+        <source>Your language is not available? You are welcome to support this project by translating it on my self hosted Weblate server.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message id="id-sources">
+        <source>Sources</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message id="id-donations">
+        <source>Donations</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message id="id-buy-me-a-beer">
+        <source>If you like my work why not buy me a beer?</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message id="id-donate-paypal">
+        <source>Donate with PayPal</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message id="id-donate-liberpay">
+        <source>Donate with Liberpay</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message id="id-reset">
+        <source>Reset</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message id="id-new-game">
+        <source>New game</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message id="id-placeholder-text">
+        <source>Want to play?</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message id="id-placeholder-hint">
+        <source>Pull down to start a new game</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message id="id-reset-game">
+        <source>Reset game</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message id="id-generating">
+        <source>Generating</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message id="id-cells-unsolved" numerus="yes">
+        <source>%n cell(s) unsolved</source>
+        <translation type="unfinished">
+            <numerusform></numerusform>
+            <numerusform></numerusform>
+            <numerusform></numerusform>
+        </translation>
+    </message>
+    <message id="id-has-errors">
+        <source>There are errors</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message id="id-solved">
+        <source>Solved</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message id="id-generate">
+        <source>Generate</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message id="id-dialog-new-game-info">
+        <source>You can select a level of difficulty for your Sudoku puzzle.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message id="id-easy">
+        <source>Easy</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message id="id-medium">
+        <source>Medium</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message id="id-hard">
+        <source>Hard</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message id="id-insane">
+        <source>Insane</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message id="id-level-of-difficulty">
+        <source>Level of difficulty</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message id="id-settings">
+        <source>Settings</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message id="id-support-tools">
+        <source>Support tools</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message id="id-highlighting">
+        <source>Highlighting</source>
+        <oldsource>Prevent display blanking</oldsource>
+        <translation type="unfinished"></translation>
+    </message>
+    <message id="id-highlighting-desc">
+        <source>This option enables highlighting of rows, columns and boxes already blocked by the selected number.</source>
+        <oldsource>This option prevents display blanking during playing.</oldsource>
+        <translation type="vanished">This option enables highlighting of rows, columns and boxes already blocked by the selected number.</translation>
+    </message>
+    <message id="id-sudoku-board">
+        <source>Sudoku board</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message id="id-level">
+        <source>Level</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message id="id-general">
+        <source>General</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message id="id-auto-notes">
+        <source>Auto notes</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message id="id-auto-notes-desc">
+        <source>This option automatically adds all notes to a new sudoku puzzle.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message id="id-display-blanking">
+        <source>Prevent display blanking</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message id="id-display-blanking-desc">
+        <source>This option prevents display blanking during playing.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message id="id-auto-clean">
+        <source>Auto clean</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message id="id-auto-clean-desc">
+        <source>This option enables auto cleaning of notes when a number is added to a cell.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message id="id-highlight-combo-desc">
+        <source>Choose a highlighting mode to highlight a single cell with the selected number or the row, column and block the selected number affects.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message id="id-none">
+        <source>None</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message id="id-cell">
+        <source>Cell</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message id="id-row-column-block">
+        <source>Row / column / block</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message id="id-styles-desc">
+        <source>Choose a style for you sudoku board.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message id="id-style">
+        <source>Style</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message id="id-default">
+        <source>Default</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message id="id-style-black-and-white">
+        <source>Black &amp; White</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message id="id-style-paper">
+        <source>Paper</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message id="id-puzzle-solved">
+        <source>Puzzle solved!</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message id="id-steps-required">
+        <source>Steps required</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message id="id-time-required">
+        <source>Time required</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message id="id-hints-used">
+        <source>Hints used</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message id="id-generator-failed-msg">
+        <source>Generation of Sudoku game failed! Please try again!</source>
+        <oldsource>Generation of game failed! Please try again!</oldsource>
+        <translation type="unfinished"></translation>
+    </message>
+    <message id="id-reset-settings">
+        <source>Reset settings</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message id="id-resetting-settings">
+        <source>Resetting settings</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+</TS>
